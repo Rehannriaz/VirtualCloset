a2d206b5b855140c3cb3b704d3544f58db509045
ArsalShaikh12 <101574226+ArsalShaikh12@users.noreply.github.com>
2023-04-18 07:33:54 +0500

    Update passport-config.js

--- passport-config.js
+++ passport-config.js
@@ -1,27 +1,53 @@
-// const localStrategy = require ("passport-local").Strategy
-// const bcrypt =require ('bcrypt')
+const localStrategy = require("passport-local").Strategy;
+const { pool } = require('./db.js');
+const bcrypt = require("bcrypt");
 
-// function initialize(passport,getUserByUsername){
-//     // function to authenticate users
-//     const authenticateUsers=async(username,password, done) => {
-//         // get users by username
-//         const user1=getUserByUsername(username)
-//         if(user1== null){
-//             return done(null,false,{message: "User/Pass incorrect"})
-//         }
-//         try{
-//             if(await bcrypt.compare(password,user1.password)){
-//                 return done(null,user1)
-//             }
-//             else{
-//                 return done(null,false,{message:"User/Pass incorrect"})
-//             }
-//         } catch(e) {
-//             console.log(e);
-//             return done(e);
-//         }
+function initialize(passport) {
+  const authenticateUser = async (username, password, done) => {
+    try {
+      const query = 'SELECT * FROM users WHERE email = $1';
+      const { rows } = await pool.query(query, [username]);
+      if (rows.length > 0) {
+        const user = rows[0];
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Incorrect password" });
+        }
+      } else {
+        return done(null, false, { message: "Email is not registered" });
+      }
+    } catch (error) {
+      console.error('Error fetching user from database:', error);
+      return done(error);
+    }
+  }
 
-//     }
-//     passport.use(new localStrategy({usernameField: 'username'},authenticateUsers))
-// }
-// module.exports = initialize
+  passport.use(new localStrategy({
+    usernameField: "email",
+    passwordField: "password"
+  }, authenticateUser));
+
+  passport.serializeUser((user, done) => {
+    done(null, user.userid);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const query = 'SELECT * FROM users WHERE userid = $1';
+      const { rows } = await pool.query(query, [id]);
+      if (rows.length > 0) {
+        const user = rows[0];
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "User not found" });
+      }
+    } catch (error) {
+      console.error('Error fetching user from database:', error);
+      return done(error);
+    }
+  });
+}
+
+module.exports = initialize;
