99140c63d1978246cf4b64702c665492179cdaf1
adeen amir <100951301+adeen08@users.noreply.github.com>
2023-04-21 01:14:45 +0500

    Add files via upload

---
+++ src/student/outfit.js
@@ -0,0 +1,24 @@
+function openTab(evt, TabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("head2");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(TabName).style.display = "block";
+    evt.currentTarget.className += " active";
+    
+}
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("OpenAuto").click();
+
